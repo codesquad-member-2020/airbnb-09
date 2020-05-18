e10e60047c1f36c3f3bebbc6641572fcc7e87be6
blossun <dnsngu@gmail.com>
2020-05-19 04:57:53 +0900

    [#7] Chore : 프로젝트 초기화
    - 스프링 부트 프로젝트 초기화
    - gitignore 파일 추가

---
+++ BE/src/main/java/kr/codesquad/airbnb09/Airbnb09Application.java
@@ -0,0 +1,13 @@
+package kr.codesquad.airbnb09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airbnb09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Airbnb09Application.class, args);
+	}
+
+}
