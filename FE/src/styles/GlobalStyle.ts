51d5cd20edd09aaa6b8af4875a92b9e1a556c917
Reese Kim <reesekimm@gmail.com>
2020-05-21 15:59:22 +0900

    [#1] Chore : GlobalStyle 수정

--- FE/src/styles/GlobalStyle.ts
+++ FE/src/styles/GlobalStyle.ts
@@ -9,7 +9,6 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-    color: blue;
   }
 `;
 
