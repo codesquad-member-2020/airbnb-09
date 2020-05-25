1b9967da20db111a889a6aae12bd6b6042f8ab5d
Sungik Choi <tjddlr92@gmail.com>
2020-05-25 16:12:37 +0900

    [#17] Feat : mock data 사용하여 card list 렌더링 테스트
    - 우선은 Context API 사용하지 않고 props를 넘겨줘서 렌더링 테스트

--- FE/src/components/CardList/Card/Description.jsx
+++ FE/src/components/CardList/Card/Description.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Description = () => {
-  return <div></div>;
+const Description = ({ name, country, rating, superHost, originalRate, sellingRate }) => {
+  return (
+    <div>
+      <span>{name}</span>
+      <span>{country}</span>
+      {superHost && <div>슈퍼호스트</div>}
+      <span>{rating}</span>
+      <span>{originalRate}</span>
+      <span>{sellingRate}</span>
+    </div>
+  );
 };
 
 export default Description;
--- FE/src/components/CardList/Card/ImageSlider.jsx
+++ FE/src/components/CardList/Card/ImageSlider.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const ImageSlider = () => {
-  return <div></div>;
+const ImageSlider = ({ thumbnails }) => {
+  return (
+    <div>
+      {thumbnails.map((imageUrl, index) => (
+        <img key={index} src={imageUrl} alt="숙소 이미지" />
+      ))}
+    </div>
+  );
 };
 
 export default ImageSlider;
