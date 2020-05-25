d70ba0004c4f25994bd0a695ba84c20ffbb5536a
Reese Kim <reesekimm@gmail.com>
2020-05-25 16:41:36 +0900

    [#28] Feat : Header 초기 스타일 적용
    - logo.svg 파일 사용하여 로고 렌더링
    - svg 파일 핸들링을 위한 webpack loader 추가

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import Button from "Styles/Button";
+import logo from "../../assets/logo.svg";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+const Header = () => (
+  <Wrapper>
+    <Logo type="image/svg+xml" data={logo} />
+    <Button rounded shadow>
+      Login
+    </Button>
+  </Wrapper>
+);
+
+const Wrapper = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.object`
+  width: 100px;
+`;
 
 export default Header;
--- FE/webpack.common.js
+++ FE/webpack.common.js
@@ -18,6 +18,17 @@ module.exports = {
         },
         exclude: /node_modules/,
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
