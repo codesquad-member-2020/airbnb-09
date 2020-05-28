27f6ff0321a89dcec7dcc7cf2beaa83a15e18d41
Sungik Choi <tjddlr92@gmail.com>
2020-05-28 14:22:34 +0900

    [#16] Fix : Image 사이즈 고정되게 수정
    - 이미지 사이즈가 다 다르므로 알맞게 잘라서 보여지도록 수정

--- FE/src/components/CardList/Card/ImageSlider.jsx
+++ FE/src/components/CardList/Card/ImageSlider.jsx
@@ -3,17 +3,31 @@ import styled from "styled-components";
 
 const ImageSlider = ({ thumbnails }) => {
   return (
-    <div>
+    <ImageWrapper>
       {/* {thumbnails.map((imageUrl, index) => (
         <img key={index} src={imageUrl} alt="숙소 이미지" />
       ))} */}
       <Image src="/" />
-    </div>
+    </ImageWrapper>
   );
 };
 
+const ImageWrapper = styled.div`
+  width: 100%;
+  padding-top: 66.6667%;
+  overflow: hidden;
+  position: relative;
+`;
+
 const Image = styled.img`
-  max-width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  background-color: ${props => props.theme.colors.gray6};
   border-radius: ${props => props.theme.spacings.xxsm};
 `;
 
