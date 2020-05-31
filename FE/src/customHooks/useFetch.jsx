c74483723a61492f2bc4612e7fe62b5b1f9e4bb0
Reese Kim <reesekimm@gmail.com>
2020-05-31 15:17:00 +0900

    [#41] Refactor : useFetch 함수명 수정
    getInitialData -> getData

--- FE/src/customHooks/useFetch.jsx
+++ FE/src/customHooks/useFetch.jsx
@@ -5,7 +5,7 @@ const useFetch = ({ url, dispatch, actionType: { success, error } }) => {
   const [loading, setLoading] = useState(true);
   const [errorMsg, setErrorMsg] = useState(null);
 
-  const getInitialData = async () => {
+  const getData = async () => {
     const { data } = await axios.get(url);
     try {
       dispatch({ type: success, payload: data });
@@ -17,7 +17,7 @@ const useFetch = ({ url, dispatch, actionType: { success, error } }) => {
   };
 
   useEffect(() => {
-    getInitialData();
+    getData();
   }, []);
 
   return { loading, errorMsg };
