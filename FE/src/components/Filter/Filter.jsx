405ffa18da94e0aa41f9695b9e4f7937c45dd446
Sungik Choi <tjddlr92@gmail.com>
2020-06-01 17:09:19 +0900

    [#48] Refactor : 카드 리스트 필터링 요청 로직 useFetch 사용하도록 변경
    - useFetch 옵션값을 줘서 재사용하도록 변경
    - usePrevious 파일 별도로 분리
    - util 파일 생성 후 객체 비교하는 유틸함수들 분리

--- FE/src/components/Filter/Filter.jsx
+++ FE/src/components/Filter/Filter.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from "react";
-import axios from "axios";
+import React, { useContext } from "react";
 import { filterInitialState } from "InitialStates/initialStates";
 import { FilterContext } from "Contexts/filterContext";
 import { CardListContext } from "Contexts/cardListContext";
 import { fetchActions, filterActions } from "Actions/actions";
+import { isSameObject, isSameValue } from "Utils/utils";
 import useFetch from "CustomHooks/useFetch";
+import usePrevious from "CustomHooks/usePrevious";
 import styled from "styled-components";
 import Date from "./Date/Date";
 import Guest from "./Guest/Guest";
@@ -13,22 +14,19 @@ import Price from "./Price/Price";
 const Filter = () => {
   const { queries, filterDispatch } = useContext(FilterContext);
   const { cardListDispatch } = useContext(CardListContext);
-
   const { checkin, checkout, adults, children, infants, priceMin, priceMax } = queries;
+  const previousQueries = usePrevious(queries);
+  const isValidRequest = !(isSameValue(queries, filterInitialState) || isSameObject(queries, previousQueries));
 
-  // Todo:
-  // ! queries가 같을 때 재요청을 보내지 않도록 처리
-  // ! useFetch 사용 여부 확인
+  const fetchOptions = {
+    url: `http://3.34.15.148/api/listing/search?checkin=${checkin}&checkout=${checkout}&adults=${adults}&children=${children}&infants=${infants}&priceMin=${priceMin}&priceMax=${priceMax}`,
+    dispatch: cardListDispatch,
+    actionType: { success: fetchActions.FETCH_SUCCESS },
+    state: queries,
+    isValidRequest,
+  };
 
-  useEffect(() => {
-    if (queries === filterInitialState) return;
-    const getData = async () => {
-      const url = `http://3.34.15.148/api/listing/search?checkin=${checkin}&checkout=${checkout}&adults=${adults}&children=${children}&infants=${infants}&priceMin=${priceMin}&priceMax=${priceMax}`;
-      const { data } = await axios.get(url);
-      cardListDispatch({ type: fetchActions.FETCH_SUCCESS, payload: data });
-    };
-    getData();
-  }, [queries]);
+  useFetch(fetchOptions);
 
   const filterByGuest = payload => filterDispatch({ type: filterActions.FILTER_BY_GUEST, payload });
 
---
+++ FE/src/utils/utils.js
@@ -0,0 +1,2 @@
+export const isSameObject = (value, other) => JSON.stringify(value) === JSON.stringify(other);
+export const isSameValue = (value, other) => value === other;
