6d13f9db5b96afc64fb7a9ff8d9dcacbaea78b86
최성익 <tjddlr92@gmail.com>
2020-06-03 01:42:53 +0900

    [#31] Feat : Date 내부에서 사용하는 유틸 함수 분리
    - 버튼 텍스트를 월, 일로 바꿔주는 함수
    - 쿼리에 적합한 형식으로 날짜를 바꿔주는 함수

--- FE/src/utils/utils.js
+++ FE/src/utils/utils.js
@@ -1,2 +1,7 @@
 export const isSameObject = (value, other) => JSON.stringify(value) === JSON.stringify(other);
+
 export const isSameValue = (value, other) => value === other;
+
+export const toMonthDayString = str => `${str.month() + 1}월 ${str.date()}일`;
+
+export const formatDate = date => date.format("YYYY[-]MM[-]DD");
