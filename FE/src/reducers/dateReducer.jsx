866e676d7755365dd91ccb041297dfbb71a448e5
최성익 <tjddlr92@gmail.com>
2020-06-03 01:03:24 +0900

    [#31] Fix : startDate, endDate 값 읽지 못하는 버그 수정
    - 구조 분해 할당을 하니, 리셋하는 경우에는 별도의 payload가 없어 버그가 발생했다. 분해할당을 없애니 제대로 동작함.

--- FE/src/reducers/dateReducer.jsx
+++ FE/src/reducers/dateReducer.jsx
@@ -2,16 +2,13 @@ import { SET_DATE, RESET_DATE } from "Actions/dateAction";
 import { dateInitialState } from "InitialStates/initialStates";
 
 const dateReducer = (state, action) => {
-  const {
-    type,
-    payload: { startDate, endDate },
-  } = action;
+  const { type, payload } = action;
 
   switch (type) {
     case SET_DATE:
-      return { ...state, startDate, endDate };
+      return { ...state, startDate: payload.startDate, endDate: payload.endDate };
     case RESET_DATE:
-      return { ...state, ...dateInitialState };
+      return { ...dateInitialState };
     default:
       return state;
   }
