2fbad3a5101534192cc48f9ef2da0258ac63253c
최성익 <tjddlr92@gmail.com>
2020-06-02 22:18:29 +0900

    [#31] Feat : Data.jsx 기본 설정
    - react-dates 모달 안에 나타나게 기본 설정 적용

--- FE/src/components/Filter/Date/Date.jsx
+++ FE/src/components/Filter/Date/Date.jsx
@@ -1,8 +1,38 @@
-import React from "react";
-import DateButton from "./DateButton";
+import React, { useState, useReducer } from "react";
+import styled from "styled-components";
+import "react-dates/initialize";
+import { DayPickerRangeController } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+import guestReducer from "Reducers/guestReducer";
+import { guestInitialState } from "InitialStates/initialStates";
+import FilterButton from "../FilterButton";
+import Modal from "../Modal";
 
-const Date = () => {
-  return <DateButton />;
+const Date = ({ dispatchHandler }) => {
+  const [toggle, setToggle] = useState(false);
+  const [guestNum, dispatch] = useReducer(guestReducer, guestInitialState);
+
+  const setFilterState = () => {
+    setToggle(!toggle);
+  };
+
+  const modalContent = <DayPickerRangeController />;
+
+  const modalOptions = {
+    contents: modalContent,
+    toggleHandler: () => setToggle(false),
+  };
+
+  return (
+    <GuestWrapper>
+      <FilterButton clickHandler={setFilterState} active={toggle} text="날짜" />
+      {toggle && <Modal options={modalOptions} />}
+    </GuestWrapper>
+  );
 };
+
+const GuestWrapper = styled.div`
+  position: relative;
+`;
 
 export default Date;
