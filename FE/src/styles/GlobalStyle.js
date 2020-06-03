ae4e3b4066ae196e42aa2b49f08fc611c3696e00
최성익 <tjddlr92@gmail.com>
2020-06-03 15:59:05 +0900

    [#56] Feat : 카드 리스트 스타일 수정
    - 폰트 적용이 제대로 안되고 있어 GlobalStyle에서 font-family 속성 이동
    - 필터 버튼 텍스트 사이즈 증가
    - 카드리스트 가격 사이즈 제대로 적용이 안되고 있어 개별로 적용해줌

--- FE/src/styles/GlobalStyle.js
+++ FE/src/styles/GlobalStyle.js
@@ -5,6 +5,7 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   * {
     box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji' !important;
     font-size: 16px;
     &:before, &:after {
       box-sizing: border-box;
@@ -13,10 +14,6 @@ const GlobalStyle = createGlobalStyle`
 
   a {
     cursor: pointer;
-  }
-
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
   }
 `;
 
