5b59755556c3ec5a0e941dc4e5e835bec6d4891d
blossun <dnsngu@gmail.com>
2020-06-03 15:00:41 +0900

    [#55] Feat : JWT 토큰이 없을 경우 예외 처리 추가

--- BE/src/main/java/kr/codesquad/airbnb09/common/error/GlobalExceptionHandler.java
+++ BE/src/main/java/kr/codesquad/airbnb09/common/error/GlobalExceptionHandler.java
@@ -13,4 +13,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleSignatureException(SignatureException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유요하지 않은 토큰값입니다.");
     }
+
+    @ExceptionHandler(TokenNotFoundException.class)
+    public ResponseEntity<String> handleTokenNotFoundException(TokenNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
 }
