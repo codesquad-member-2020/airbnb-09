d886242eb0125e8b34ed997cf034c88ad9df6fe2
최성익 <tjddlr92@gmail.com>
2020-06-03 19:30:16 +0900

    [#61] Feat : 현재 날짜 이전 선택 불가능 기능 구현
    - react-dates 라이브러리 내부에 있는 유틸 함수 활용
    - 현재 ~ 1년뒤 까지만 달력 이동할 수 있도록 설정

--- FE/src/components/Filter/Date/DateModal.jsx
+++ FE/src/components/Filter/Date/DateModal.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import moment from "moment";
 import "moment/locale/ko";
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
 import { DayPickerRangeController } from "react-dates";
 import { START_DATE } from "react-dates/constants";
+import isInclusivelyAfterDay from "react-dates/src/utils/isInclusivelyAfterDay";
 import { setDate, resetDate } from "Actions/dateAction";
 import DatesWrapper from "Styles/DatesWrapper";
 import Modal from "../Modal";
@@ -14,6 +16,9 @@ const DateModal = ({ setToggle, dateState, dateDispatch }) => {
 
   const onDatesChange = date => dateDispatch(setDate(date));
   const onFocusChange = focusedInput => setFocus(!focusedInput ? START_DATE : focusedInput);
+  const isOutsideRange = day => !isInclusivelyAfterDay(day, moment());
+  const now = moment();
+  const nextYear = moment().clone().add(1, "year");
 
   const modalContent = (
     <DatesWrapper>
@@ -21,9 +26,12 @@ const DateModal = ({ setToggle, dateState, dateDispatch }) => {
         numberOfMonths={2}
         startDate={startDate}
         endDate={endDate}
+        minDate={now}
+        maxDate={nextYear}
         onDatesChange={onDatesChange}
         focusedInput={focus}
         onFocusChange={onFocusChange}
+        isOutsideRange={isOutsideRange}
       />
     </DatesWrapper>
   );
