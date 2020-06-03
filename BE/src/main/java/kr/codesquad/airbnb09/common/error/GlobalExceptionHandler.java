ee438297cf0e44188d05e3af4e3c17c09405c379
blossun <dnsngu@gmail.com>
2020-06-03 12:30:38 +0900

    [#55] Feat : SignatureException 처리 추가
    유효하지않은 토큰값 에러 처리

---
+++ BE/src/main/java/kr/codesquad/airbnb09/common/error/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package kr.codesquad.airbnb09.common.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.security.SignatureException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<String> handleSignatureException(SignatureException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유요하지 않은 토큰값입니다.");
+    }
+}
