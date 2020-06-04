a86e7fbeb882d1e14131dea6fd1aeccd7ca56296
blossun <dnsngu@gmail.com>
2020-06-05 02:39:56 +0900

    [#71] Feat : 페이징 구현
    모든 조회 쿼리에 limit, offset 추가
    request에 limit, offset값이 전달되지 않는 경우 default값 설정

--- BE/src/main/java/kr/codesquad/airbnb09/service/ListingMapper.java
+++ BE/src/main/java/kr/codesquad/airbnb09/service/ListingMapper.java
@@ -11,10 +11,12 @@ import java.util.List;
 @Mapper
 public interface ListingMapper {
 
-    List<AccommodationVO> selectAllListing(@Param("count") int count);
+    List<AccommodationVO> selectAllListing(@Param("limit") Integer limit, @Param("offset") Integer offset);
     List<AccommodationVO> filterListingByDate(@Param("date_checkin") LocalDate checkin, @Param("date_checkout") LocalDate checkout,
                                               @Param("total_personnel") Integer accommodates,
-                                              @Param("min_price") Integer minPrice, @Param("max_price") Integer maxPrice);
-    List<AccommodationVO> filterListingByAccommodates(@Param("total_personnel") Integer accommodates);
+                                              @Param("min_price") Integer minPrice, @Param("max_price") Integer maxPrice,
+                                              @Param("limit") Integer limit, @Param("offset") Integer offset);
+    List<AccommodationVO> filterListingByAccommodates(@Param("total_personnel") Integer accommodates,
+                                                      @Param("limit") Integer limit, @Param("offset") Integer offset);
     List<String> selectImageUrlsByListingId(@Param("listing_id") Long listingId);
 }
