99873720ffb1366f21d220b1d8f6a7d82efa1cbe
최성익 <tjddlr92@gmail.com>
2020-06-04 16:31:59 +0900

    [#65] Feat : 예약 모달 구현 완료

--- FE/src/components/CardList/Card/ReservationModal.jsx
+++ FE/src/components/CardList/Card/ReservationModal.jsx
@@ -1,25 +1,110 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import Button from "Styles/Button";
 import Text from "Styles/Text";
+import { IconContext } from "react-icons";
+import { MdClose } from "react-icons/md";
+import { FilterContext } from "Contexts/filterContext";
+import { SELLING_RATE_TEXT } from "Constants/constants";
+import { addedWonUnitRate, renderGuestButtonText } from "Utils/utils";
+import Rating from "./Rating";
 
-const ReservationModal = ({ data, clickHandler }) => {
+const ReservationModal = ({
+  data: {
+    oneNightRate: { selling },
+    nights,
+    rating,
+    price: { accomdationRate, cleaningFee, serviceFee, totalPrice },
+  },
+  clickHandler,
+}) => {
+  const {
+    queries: { checkin, checkout, adults, children, infants },
+  } = useContext(FilterContext);
   const RESERVATION_BUTTON_TEXT = "예약하기";
   const NOTIFICATION_TEXT = "예약 확정 전에는 요금이 청구되지 않습니다";
+  const reservationConfirmHandler = () => {
+    alert("숙소가 예약되었습니다.");
+    clickHandler();
+  };
+
+  const guestList = { adults, children, infants };
+
+  const filterList = [
+    {
+      id: "date",
+      type: "날짜",
+      value: `${checkin} ~ ${checkout}`,
+    },
+    {
+      id: "guest",
+      type: "인원",
+      value: renderGuestButtonText(guestList),
+    },
+  ];
+
+  const priceList = [
+    {
+      id: "accomdationRate",
+      type: `${addedWonUnitRate(selling)} x ${nights}박`,
+      price: `${addedWonUnitRate(accomdationRate)}`,
+    },
+    {
+      id: "cleaningFee",
+      type: "청소비",
+      price: `${addedWonUnitRate(cleaningFee)}`,
+    },
+    {
+      id: "serviceFee",
+      type: "서비스 수수료",
+      price: `${addedWonUnitRate(serviceFee)}`,
+    },
+    {
+      id: "totalPrice",
+      type: "총합",
+      price: `${addedWonUnitRate(totalPrice)}`,
+    },
+  ];
 
   return (
     <>
       <DimmedLayer onClick={clickHandler} />
       <ModalWrapper>
-        <Text>날짜</Text>
-        <div />
-        <Text>인원</Text>
-        <div />
-        <div>
-          <Text>무슨무슨돈</Text>
-          <Text>얼마얼마얼마</Text>
-        </div>
-        <ReservationButton primary onClick={clickHandler}>
+        <CloseButton circular highlighted onClick={clickHandler}>
+          <IconContext.Provider value={{ size: "1.5rem" }}>
+            <MdClose />
+          </IconContext.Provider>
+        </CloseButton>
+        <PrimaryInfoWrapper>
+          <PriceTextWrap>
+            <SellingPriceText fontSize="xl" fontWeight="bold">
+              {addedWonUnitRate(selling)}
+            </SellingPriceText>
+            <Text>{SELLING_RATE_TEXT}</Text>
+          </PriceTextWrap>
+          <Rating color="green" ratingValue={rating} />
+        </PrimaryInfoWrapper>
+        {filterList.map(({ id, type, value }) => {
+          return (
+            <div key={id}>
+              <Text fontWeight="semiBold">{type}</Text>
+              <InputWrapper>
+                <Text>{value}</Text>
+              </InputWrapper>
+            </div>
+          );
+        })}
+        <PriceListWrapper>
+          {priceList.map(({ id, type, price }) => {
+            return (
+              <PriceWrapper key={id}>
+                <Text>{type}</Text>
+                <Text>{price}</Text>
+              </PriceWrapper>
+            );
+          })}
+        </PriceListWrapper>
+        <ReservationButton primary onClick={reservationConfirmHandler}>
           {RESERVATION_BUTTON_TEXT}
         </ReservationButton>
         <NotificationWrapper>
@@ -41,15 +126,67 @@ const DimmedLayer = styled.div`
   opacity: 0.5;
 `;
 
+const CloseButton = styled(Button)`
+  position: absolute;
+  top: ${({ theme }) => theme.spacings.sm};
+  right: ${({ theme }) => theme.spacings.sm};
+`;
+
+const PrimaryInfoWrapper = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray5};
+  padding-bottom: ${({ theme }) => theme.spacings.sm};
+  margin-bottom: ${({ theme }) => theme.spacings.sm};
+`;
+
 const ReservationButton = styled(Button)`
   width: 100%;
   font-size: ${({ theme }) => theme.fontSizes.lg};
   padding: ${({ theme }) => theme.spacings.sm} 0;
 `;
 
+const PriceTextWrap = styled.div`
+  margin-bottom: ${({ theme }) => theme.spacings.xsm};
+`;
+
+const SellingPriceText = styled(Text)`
+  margin-right: ${({ theme }) => theme.spacings.unit(0.5)};
+`;
+
 const NotificationWrapper = styled.div`
   padding-top: ${({ theme }) => theme.spacings.sm};
   text-align: center;
+`;
+
+const InputWrapper = styled.div`
+  width: 100%;
+  margin-top: ${({ theme }) => theme.spacings.xsm};
+  margin-bottom: ${({ theme }) => theme.spacings.sm};
+  padding: ${({ theme }) => theme.spacings.sm};
+  border: 1px solid ${({ theme }) => theme.colors.gray5};
+  ${Text} {
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+  }
+`;
+
+const PriceWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: ${({ theme }) => theme.spacings.sm} 0;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray5};
+  ${Text} {
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+  }
+`;
+
+const PriceListWrapper = styled.div`
+  padding-bottom: ${({ theme }) => theme.spacings.sm};
+  ${PriceWrapper}:last-child {
+    border-bottom: none;
+    ${Text} {
+      font-weight: ${({ theme }) => theme.fontWeights.semiBold};
+    }
+  }
 `;
 
 const ModalWrapper = styled.div`
@@ -60,7 +197,7 @@ const ModalWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   width: 100%;
-  max-width: ${({ theme }) => theme.spacings.unit(150)};
+  max-width: ${({ theme }) => theme.spacings.unit(120)};
   padding: ${({ theme }) => theme.spacings.lg};
   border-radius: ${({ theme }) => theme.spacings.xsm};
   box-shadow: ${({ theme }) => theme.shadows.xl};
